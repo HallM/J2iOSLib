d476f9825961746dfbe3d706128b9f30ca17aebc
Matthew Hall <mdhall@gmail.com>
2014-12-19 00:12:31 -0600

    Added ability to set J2ObjC path, added easier method to add/change arch’s for sim and device, fixed some bugs, added a way to drop out if a command fails.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# where J2ObjC is located if not in PATH
+J2OBJCPATH=
+
 # directory where the source files are located
 SRCDIR=src
 
@@ -26,18 +29,39 @@ IOS_DEPLOY_TGT=7.0
 # the location of XCode on the system
 XCODEROOT=/Applications/Xcode.app/Contents/Developer/Platforms
 
+# the architectures required for both simulator and device
+SIMARCHS="i386 x86_64"
+DEVARCHS="armv7 armv7s arm64"
+
 # no need to change the rest
 SIMDEVELROOT=$XCODEROOT/iPhoneSimulator.platform/Developer
 DEVDEVELROOT=$XCODEROOT/iPhoneOS.platform/Developer
+
+checkret() {
+  if [ $? -ne 0 ]
+  then
+    echo $1
+    exit 127
+  fi
+}
 
 rm -rf ${STGDIR}
 mkdir ${STGDIR}
 
 # find all java files
+echo "Searching for files in $SRCDIR"
 jfiles=`find ${SRCDIR} -name "*.java"`
 
 # transpile using defaults
-j2objc ${J2OBJCARGS} -d ${STGDIR} $jfiles
+echo "Translating Java code into ObjC"
+if [ -z "$J2OBJCPATH" ]
+then
+  j2objc ${J2OBJCARGS} -d ${STGDIR} $jfiles
+else
+  ${J2OBJCPATH}/j2objc ${J2OBJCARGS} -d ${STGDIR} $jfiles
+fi
+
+checkret "Failed to translate code"
 
 # use some string replace to determine the m files and eventually the o files
 # also to prevent collisions with pkg1/file.java and pkg2/file.java, including pkg name in the o file
@@ -46,15 +70,25 @@ ofiles=${jfiles//.\//}
 ofiles=${ofiles//.java/.o}
 ofiles=${ofiles//\//_}
 
-# first compiling the simulator versions
-DEVROOT=$SIMDEVELROOT
-SDKROOT=${DEVROOT}/SDKs/iPhoneSimulator${IOS_BASE_SDK}.sdk
-
 cd ${STGDIR}
 
+allarchs="$SIMARCHS $DEVARCHS"
 lipoargs=""
-for outarch in x86_64 i386 armv7 armv7s arm64
+for outarch in $allarchs
 do
+  if [[ $SIMARCHS =~ $outarch ]]
+  then
+    DEVROOT=$SIMDEVELROOT
+    SDKROOT=${DEVROOT}/SDKs/iPhoneSimulator${IOS_BASE_SDK}.sdk
+  elif [[ $DEVARCHS =~ $outarch ]]
+  then
+	DEVROOT=$DEVDEVELROOT
+  	SDKROOT=${DEVROOT}/SDKs/iPhoneOS${IOS_BASE_SDK}.sdk
+  else
+    echo "$outarch is not in the list of simulator nor device list. Skipping arch."
+    continue
+  fi
+  
   CFLAGS="-arch $outarch -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -ObjC -I./"
 
   echo "Compiling for architecture: $outarch"
@@ -65,23 +99,28 @@ do
     ofile=${ofile/.\//}
     ofile=${ofile//\//_}
     echo "Compiling $mfile"
-    j2objcc $CFLAGS -c -o ${ofile/.\//} $mfile
+
+    if [ -z "$J2OBJCPATH" ]
+    then
+      j2objcc $CFLAGS -c -o ${ofile/.\//} $mfile
+    else
+      ${J2OBJCPATH}/j2objcc $CFLAGS -c -o ${ofile/.\//} $mfile
+    fi
+    
+    checkret "Failed to compile source code file"
   done
 
   echo "Building ${LIBNAME}_${outarch}.a"
   libtool -static -o ${LIBNAME}_${outarch}.a $ofiles
-  lipoargs="$lipoargs -arch $outarch ${LIBNAME}_${outarch}.a"
+  checkret "Failed to generate static library for arch"
 
-  # after all the simulator ones, switch to the device SDK
-  if [ "$outarch" = "i386" ]
-  then
-	DEVROOT=$DEVDEVELROOT
-  	SDKROOT=${DEVROOT}/SDKs/iPhoneOS${IOS_BASE_SDK}.sdk
-  fi
+  lipoargs="$lipoargs -arch $outarch ${LIBNAME}_${outarch}.a"
 done
 
+rm ${LIBNAME}.a
 echo "combining all into single ${LIBNAME}.a"
-lipo $lipoargs -create -output ../${LIBNAME}.a
+lipo $lipoargs -create -output ${LIBNAME}.a
+checkret "Failed to generate final static library"
 
 # clean up
 rm *.o
